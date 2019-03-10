984098bac182dce6adca9efeccce397f23ed2815
David <davidivad96@yahoo.es>
2019-03-10 23:30:54 +0100

    Improving functionality.
    Added fridge. You can open and close the door.
    
    Implemented functionality to grab objects. You can grab the milk from the fridge and fill the cup with it.
    
    Tested and succeeded app in Android.

--- VReab/Assets/Project/Scripts/Movement.cs
+++ VReab/Assets/Project/Scripts/Movement.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	private GameObject Camera;
+    private Rigidbody rigidbody_component;
 	public float speed = 1.0f;
+    private bool grabbing_something;
 
 	// Use this for initialization
 	void Start () {
 		Camera = GameObject.Find ("Main Camera");
-	}
+        rigidbody_component = gameObject.GetComponent<Rigidbody>();
+        grabbing_something = false;
+    }
 
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -25,4 +29,19 @@ public class Movement : MonoBehaviour {
 		// Fix the 'y' value so that the character does not fly
 		transform.position = new Vector3 (transform.position.x, 1.3f, transform.position.z);
 	}
+
+    // Prevent from bouncing when the player collides
+    void OnCollisionEnter(Collision collision) {
+        rigidbody_component.velocity = Vector3.zero;
+    }
+
+    // Set the grabbing_something attribute
+    public void setGrabbingSomething(bool b) {
+        grabbing_something = b;
+    }
+
+    // Get the grabbing_something attribute
+    public bool getGrabbingSomething() {
+        return grabbing_something;
+    }
 }
---
+++ VReab/Assets/Project/Scripts/OpenDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private bool active;
+    private Animator animator_component;
+
+    // Start is called before the first frame update
+    void Start() {
+        active = false;
+        animator_component = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // If detect a "PointerClick" event
+        if ((Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)) && active) {
+            // If the door is closed then open it. If it's opened, then close it
+            if (animator_component.GetBool("Active")) {
+                animator_component.SetBool("Active", false);
+            } else {
+                animator_component.SetBool("Active", true);
+            }
+        }
+    }
+
+    // Called when there's a "PointerEnter" event
+    public void Activate() {
+        active = true;
+    }
+
+    // Called when there's a "PointerExit" event
+    public void Deactivate() {
+        active = false;
+    }
+}
